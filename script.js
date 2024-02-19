8395bdc66324a3ccad844210e84e89a1dce334ef
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-02-19 14:55:42 -0300

    Update script by standard image

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const imagemPadrao = document.getElementById("image-principal");
+imagemPadrao.src = "./assets/tenis-1-galeria.png"
+
 function mostrarImagem(img){
     const imagemGrande=document.getElementById("image-principal");
     imagemGrande.src=img.src;
